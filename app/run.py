d5a1f80aa81306d4f65c3b8881e38f5807872f02
IForberg <44228390+IForberg@users.noreply.github.com>
2018-10-29 22:26:28 +0100

    Version includes graphs

--- app/run.py
+++ app/run.py
@@ -43,6 +43,10 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    # Count number of categories
+    category_counts = df.drop(['id', 'message', 'original', 'genre'], axis=1).sum()
+    category_names = list(category_counts.index)
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -61,6 +65,23 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(x=category_names,
+                    y=category_counts
+                   )
+            ],
+            
+            'layout':{
+                'title': 'Category Distribution',
+                'yaxis':{
+                    'title': "Count"
+                },
+                'xaxis':{
+                    'title': 'Gategories'
                 }
             }
         }
